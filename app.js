8fbd12f843edf00999740e25e74dac116022ae4b
Kartikeya <kartvat3@in.ibm.com>
2016-12-29 19:53:17 +0530

    hey

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 // Add your requirements
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
+var dotenv = require('dotenv');
 
+// var appId = process.env.MY_APP_ID || "missing appId";
+// appPassword: process.env.MY_APP_SECRET || "missing app password";  
+dotenv.load()
+
+console.log(process.env.MY_APP_ID);
+// Create chat bot and add dialogs
+var connector = new builder.ChatConnector({ appId:process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET});
+var bot = new builder.UniversalBot(connector);
+
+console.log(connector);
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+// Setup Restify Server
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.PORT || 3000, function() 
@@ -9,14 +26,7 @@ server.listen(process.env.PORT || 3000, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-// Create bot dialogs
-//hope it works
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
