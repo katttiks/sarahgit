4fd8214e9e70b257ce50e20dd20c582fa97efca5
Kartikeya <kartvat3@in.ibm.com>
2016-12-30 00:09:37 +0530

    api

--- greetings.js
+++ greetings.js
@@ -2,7 +2,7 @@
 var request = require('request');
 var exports = module.exports = {};
 exports.callRAPI = function(query,callback){
-request('http://localhost:8080'+ query, function (error, response, body) {
+request('http://ibm476-pc073gqf:8080'+ query, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         callback(null,body); //print the list
     }
