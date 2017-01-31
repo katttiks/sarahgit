8c99c614021920fc4c545533e0f9e04ed4a58354
Kartikeya <kartvat3@in.ibm.com>
2017-01-31 12:16:29 +0530

    Update

--- greetings.js
+++ greetings.js
@@ -2,7 +2,7 @@
 var request = require('request');
 var exports = module.exports = {};
 exports.callRAPI = function(query,callback){
-request('https://0b9fbf6a.ngrok.io'+ query, function (error, response, body) {
+request('https://8a4c62c1.ngrok.io'+ query, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         callback(null,body); //print the list
     }
