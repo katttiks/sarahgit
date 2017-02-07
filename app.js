ed109bc14a68eaf86f86bc00abeffab41c4827e6
Kartikeya <kartvat3@in.ibm.com>
2017-02-07 11:56:30 +0530

    improved flow

--- app.js
+++ app.js
@@ -13,55 +13,48 @@ dotenv.load()
 var connector = new builder.ChatConnector({ appId:process.env.MY_APP_ID, appPassword: process.env.MY_APP_SECRET});
 var bot = new builder.UniversalBot(connector);
 
+var relevant
+var obj1
 
 bot.dialog('/', [
     function(session) {
         builder.Prompts.text(session, 'Enter your topic');
     },
+
     function(session, results) {
-    var relevant = results.response;
+    relevant = results.response;
     relevant.replace('?',"")
     relevant = relevant.replace(/ is| what|what| a | are| the| or| in| of| be| case|/gi,"")
     relevant = relevant.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-    console.log(relevant);
     greetings.callRAPI("/index?doc=" + relevant, function(err,data){
-    //console.log(s);   
-    //console.log(data[7])
     if (err) console.log(err);
     else 
     {
-        var obj1 = JSON.parse(data);
+        obj1 = JSON.parse(data);
         session.send("These are the top few documents as per your query:  ")
-        var msg
-        k = 1
-        for(i=0;i<6;i++){                  
+        for(i=0;i<5;i++){                  
         //session.send("%s", obj1[i]['url']) 
-          doc_name = obj1[i]['url'].substring(obj1[i]['url'].lastIndexOf('/') + 1, obj1[i]['url'].length)
-          linked_doc = doc_name.link(obj1[i]['url'])
-          console.log(i) 
-          msg = new builder.Message(session)
-          .attachments([
-              new builder.HeroCard(session)
-             // new builder.ThumbnailCard(session)              
-              .title(doc_name)
-              .subtitle(obj1[i]['url'])
-              .tap(
-              builder.CardAction.openUrl(session, obj1[i]['url']))                                    
-        ]);
-           //keep ranking
-           rank = obj1[i]['rank']
-         //session.send(rank)  
-        session.send(doc_name +  "<br>" + obj1[i]['url'])
-         // session.send(doc_name + "-\n" + obj1[i]['url'])   
+          doc_name = obj1[i]['url'].substring(obj1[i]['url'].lastIndexOf('/') + 1, obj1[i]['url'].length)         
+           session.send(doc_name +  "<br>" + obj1[i]['url']) 
         }
-        //session.endDialog(msg)
-        //session.send("Thank you! Do you have any other query, say hi to me.")        //console.log(data);
+        builder.Prompts.text(session, 'Did you find what you were looking for?');        //console.log(data);       
+    }
+});    
+    },
+
+    function(session, results){
+       if(results.response.indexOf('no')>-1||results.response.indexOf('not')>-1||results.response.indexOf('nope')>-1) 
+       session.send("All right, I'll render some more documents for you.")
+       session.send("Here they go: ")
        
+       for(i=5;i<9;i++){                  
+        //session.send("%s", obj1[i]['url']) 
+          doc_name = obj1[i]['url'].substring(obj1[i]['url'].lastIndexOf('/') + 1, obj1[i]['url'].length)           
+           session.send(doc_name +  "<br>" + obj1[i]['url']) 
+        }
+
     }
-    //session.send('Docs: ' + data)//console.log(data);    
-});
-    
-    }
+
 ]);
 // Setup Restify Serverblahbahblahblah
 var server = restify.createServer();
